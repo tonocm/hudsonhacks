b9e6d31954043f01a6cbba0fe57f44b9a91f4bb7
tonocm <tonocm94@hotmail.com>
2015-07-18 18:48:13 -0400

    installed twilio

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var api = require('./routes/api')
+
 
 var app = express();
 
@@ -24,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
+app.use('/api', api)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
