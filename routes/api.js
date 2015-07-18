382a1bf8dce534145589c34e95e7cbf4d02d7ab1
tonocm <tonocm94@hotmail.com>
2015-07-18 19:18:28 -0400

    twilio better

--- routes/api.js
+++ routes/api.js
@@ -12,12 +12,24 @@ var client = require('twilio')(accountSid, authToken);
 
 /* GET users listing. */
 router.get('/v1', function(req, res) {
-    client.messages.create({
-        to:'+16515556677', // Any number Twilio can deliver to
+    client.sendMessage({
+
+        to:'+16073794682', // Any number Twilio can deliver to
         from: "+16466933056", // A number you bought from Twilio and can use for outbound communication
         body: 'testing from my computer :)' // body of the SMS message
-    }, function(err, message) {
-        console.log(message.sid);
+
+    }, function(err, responseData) { //this function is executed when a response is received from Twilio
+
+        if (!err) { // "err" is an error received during the request, if any
+
+            // "responseData" is a JavaScript object containing data received from Twilio.
+            // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+            // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+
+            console.log(responseData.from); // outputs "+14506667788"
+            console.log(responseData.body); // outputs "word to your mother."
+
+        }
     });
 
     res.send('respond with a resource');
