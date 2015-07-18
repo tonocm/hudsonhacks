20a1715cfa7be585601182b8b6b54f1f1078ae26
Yogisha Dixit <ydixit@gilt.com>
2015-07-18 19:49:36 -0400

    Add an in-memory store for donations

--- routes/donations.js
+++ routes/donations.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-//var bodyParser = require('body-parser')
+var donations = [];
 
-/* GET users listing. */
+function Donation(foodName, servings, location, allergies) {
+	this.foodName = foodName;
+	this.servings = servings;
+	this.location = location;
+	this.allergies = allergies;
+}
+
+// Set up a donation
 router.post('/', function(req, res) {
-  console.log(req.body);
-  res.send(200);
+  var donationJson = req.body;
+  var newDonation = new Donation(donationJson.foodName, donationJson.servings, donationJson.location, donationJson.allergies);
+  donations.push(newDonation);
+  res.send(201);
 });
 
 module.exports = router;
