70f521c803c8f14e46a83d05082172cce381fef4
Yogisha Dixit <ydixit@gilt.com>
2015-07-18 21:44:19 -0400

    Return distance between two locations

--- routes/matching.js
+++ routes/matching.js
@@ -4,8 +4,11 @@ var router = express.Router();
 
 router.post('/', function(req, res) {
 	var locationJson = req.body;
-	findDistance(locationJson.origin, locationJson.destination);
-	res.send(200);
+	findLatLong(locationJson.origin, function(lat, lng) {
+		res.send(lat + ' ' + lng);
+	});
+	//var dist = findDistance(locationJson.origin, locationJson.destination);
+	//res.send(dist);
 });
 
 var findDistance = function (origin, destination) {
@@ -15,13 +18,41 @@ var findDistance = function (origin, destination) {
 	                    'destination=' + 
 	                    encodeURIComponent(destination) + '&' + 
 	                    'sensor=false&units=imperial';
-	request(query_string, function (error, response, body) {
+	var distance = request(query_string, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
-			console.log(body)
+			var result = JSON.parse(body);
+			var dist = result.routes[0].legs[0].distance.text 
+			console.log(dist);
+			return dist;
 		}
 	});
+	return distance;
 };
 
+var findSafeSpot = function (origin, callback) {
+	findLatLong(origin, function(lat, lng) {
+		var places_query_string = 'https://maps.googleapis.com/maps/api/place/details/json?' +
+								  'key=' + // PUT IN SOME API KEY
+								  'location=' + lat + ',' + lng + '&'
+								  'radius=' + '1610' + '&' +
+								  'opennow' + '&' +
+								  'type=' + 'convenience_store' + '&';
+		request(places_query, function (error, response, body) {
+			
+		});
+	});
+	
+};
 
+var findLatLong = function (origin, callback) {
+	var location_query_string = 'http://maps.googleapis.com/maps/api/geocode/json?' + 
+								'address=' + encodeURIComponent(origin) + '&' +
+								'sensor=false';
+	request(location_query_string, function (error, response, body) {
+		var result = JSON.parse(body);
+		var latlong = result.results[0].geometry.location;
+		return callback(latlong.lat, latlong.lng);
+	});
+};
 
 module.exports = router;
