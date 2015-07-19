b00ac888e005023333b3daadef144a15e5aff6e9
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2015-07-19 00:28:25 -0400

    rotating header

---
+++ app/scripts/components/Food.jsx
@@ -0,0 +1,50 @@
+// Food.jsx
+
+'use strict';
+
+var React = require('react');
+var Food;
+
+module.exports = Food = React.createClass({
+
+    getDefaultProps: function () {
+        return {
+            foodItems: ['../../images/food/apple-emoji.png',
+                        '../../images/food/fries-emoji.png',
+                        '../../images/food/bread-emoji.png',
+                        '../../images/food/hotpot-emoji.png',
+                        '../../images/food/donut-emoji.png',
+                        '../../images/food/melon-emoji.png',
+                        '../../images/food/pizza-emoji.png',
+                        '../../images/food/rice-emoji.png',
+                        '../../images/food/chicken-emoji.png',
+                        '../../images/food/cookie-emoji.png',
+                        '../../images/food/spaghetti-emoji.png',
+                        '../../images/food/hamburger-emoji.png']
+        };
+    },
+
+    componentDidMount: function () {
+        var image = 0;
+        var divStyle = this.refs.food.getDOMNode().style;
+        var foodList = this.props.foodItems;
+
+        setInterval(function() {
+            divStyle.backgroundImage = "url(" + foodList[image] + ")";
+            if (image < foodList.length - 1) {
+                image = image + 1;
+            }
+            else {
+                image = 0;
+            }
+        }, 5000); 
+    },
+
+    
+    render: function() {
+        return (
+            <div className="food" ref="food">
+            </div>
+        );
+    }
+});
--- app/scripts/components/Header.jsx
+++ app/scripts/components/Header.jsx
@@ -3,14 +3,20 @@
 'use strict';
 
 var React = require('react');
+var Food = require('./Food.jsx');
 var Header;
 
 module.exports = Header = React.createClass({
  render: function() {
     return (
       <div className="header">
-        <div className="info">
-            <h1>Eliminate hunger</h1>
+        <div className="logo">
+            <Food />
+            <div className="less">
+            </div>
+        </div>
+        <div className="title">
+            <h1>Hungerless</h1>
         </div>
       </div>
     );
