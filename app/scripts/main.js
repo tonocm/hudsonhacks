504f5fcd11280b62352e9fbacf3db552fd05b761
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2015-07-18 22:34:12 -0400

    header created

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,17 +2,13 @@
 /** @jsx React.DOM */
 
 'use strict';
-
-var data = [
-    {author: "Helena Wu", text: "This is one comment"},
-    {author: "Steven Iseki Martin", text: "This is another comment"}
-];
-
 var React = require('react');
 var jQuery = require('jquery');
-var CommentBox = require('./components/CommentBox.jsx')
+var Header = require('./components/Header.jsx');
 
 React.render(
-  <CommentBox data={data} />,
+  <div className="container">
+   <Header />
+  </div>,
   document.getElementById('content')
 );
---
+++ dist/scripts/main.js
@@ -0,0 +1,19 @@
+// main.js
+/** @jsx React.DOM */
+
+'use strict';
+
+var data = [
+    {author: "James Zeve Martin", text: "This is one comment"},
+    {author: "Steven Iseki Martin", text: "This is another comment"}
+];
+
+var React = require('react');
+var jQuery = require('jquery');
+var CommentBox = require('./components/CommentBox.jsx')
+
+React.render(
+  <div className="container">
+    <p>Hello</p>
+  </div>
+);
