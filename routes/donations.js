53e12bd4123dedf5892e41715ca43e8c8b1fc70c
Yogisha Dixit <ydixit@gilt.com>
2015-07-19 01:06:19 -0400

    Add code to find receiver match

--- routes/donations.js
+++ routes/donations.js
@@ -1,13 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
+var api = require('./routes/api');
+var matching = require('./routes/matching');
+
 var donations = [];
 
 // Set up a donation
 router.post('/', function(req, res) {
   var donation = JSON.parse(req.body);
+  checkForReceiverMatch(donation, function () {
+  	// found a match, do something!
+  });
   donations.push(donation);
   res.send(201);
 });
+
+var getDonations = function() {
+	return donations;
+}
+
+var checkForReceiverMatch = function (donation, callback) {
+	var receivers = api.getReceivers();
+	for (var i = 0; i < receivers.length; i++) {
+		matching.findDistance(donation.location, receivers[i].location, function(distance) {
+			if (distance <= 1.0) {
+				// check allergy information
+				var allergyMatch = false;
+				for (var j = 0; j < receivers[i].allergies.length; j++) {
+					if (donation.allergies.indexOf(receivers[i].allergies[j]) > -1) {
+						allergyMatch = true;
+						break;
+					}
+				}
+				if (allergyMatch == false) {
+					return callback();
+				}
+			}
+
+		});
+	}
+};
 
 module.exports = router;
