4d1c655c5b89ea686ae132633f052b7822e41526
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2015-07-19 06:15:12 -0400

    form

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,11 +4,13 @@ var React = require('react');
 var jQuery = require('jquery');
 var Header = require('./components/Header.jsx');
 var Body = require('./components/Body.jsx');
+var Footer = require('./components/Footer.jsx');
 
 React.render(
   <div className="container">
    <Header />
    <Body />
+   <Footer />
   </div>,
   document.getElementById('content')
 );
