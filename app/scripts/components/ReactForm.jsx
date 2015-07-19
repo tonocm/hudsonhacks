bcb5b4ee42109c1cef54a9c9194b5256a511b309
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2015-07-19 16:34:59 -0400

    lol we lost

--- app/scripts/components/ReactForm.jsx
+++ app/scripts/components/ReactForm.jsx
@@ -22,8 +22,11 @@ module.exports = ReactForm = React.createClass({
     submit: function() {
         var xmlhttp = new XMLHttpRequest();
         var data = {"name": "Andy Powell", "foodName": "Chocolate Chip Cookies","servings": 1,"location": "156 5th Avenue, New York, NY", "allergies": []};
+        var component = this;
         xmlhttp.addEventListener('load', function() {
             alert(xmlhttp.responseText);
+            component.refs.submitted.getDomNode().className = "submitted response";
+            component.refs.name.getDOMNode().innerHTML = xmlhttp.responseText.name;
         }, false);
         xmlhttp.open("POST","http://localhost:3000/donations",true);
         xmlhttp.setRequestHeader("Content-type","application/json");
@@ -33,6 +36,13 @@ module.exports = ReactForm = React.createClass({
     render: function() {
         return (
             <div className="form">
+                <div className="submitted hidden" ref="submitted">
+                    <div className="name" ref="name">
+                    </div>
+                    <div className="address" ref="address">
+                    </div>
+                    
+                </div>
                 <h1>Donate Now</h1>
                 <Form type={Donation} ref="form"/>
                 <button onClick={this.submit}>Feed Someone</button>
